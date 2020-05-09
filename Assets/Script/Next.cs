5a9c50a0a5f090d50717976a9be25a0f5cdef5af
AdityaWibisono17 <18523217@students.uii.ac.id>
2020-05-09 17:11:59 +0700

    first commit

---
+++ Assets/Script/Next.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Next : MonoBehaviour
+{
+    public void Lanjut(){
+        SceneManager.LoadScene("OverScene");
+    }
+}
---
+++ Assets/Script/Play.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource buttonSound;
+    public void Mulai(){
+        buttonSound.PlayOneShot(buttonSound.clip);
+        SceneManager.LoadScene("Puzzle(A)");
+    }
+}
